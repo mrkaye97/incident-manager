9c3c39994cb8a791381b039334c2baa531eecd48
mrkaye97 <mrkaye97@gmail.com>
2026-07-03 17:10:45 -0400

    feat: initial db setup

--- worker.py
+++ worker.py
@@ -1,4 +1,19 @@
 from hatchet_sdk import Hatchet, EmptyModel, Context
+from asyncpg import connect, Connection
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from typing import AsyncGenerator, cast
+
+
+class Settings(BaseSettings):
+    database_url: str
+
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+
+
+class Lifespan:
+    def __init__(self, conn: Connection):
+        self.conn = conn
+
 
 hatchet = Hatchet()
 
@@ -9,8 +24,20 @@ async def simple(_i: EmptyModel, _c: Context) -> None:
     return None
 
 
+async def lifespan() -> AsyncGenerator[Lifespan, None]:
+    print("Starting lifespan...")
+    settings = Settings()
+    conn = cast(Connection, await connect(dsn=settings.database_url))
+    print("Connected to database.")
+    try:
+        ls = Lifespan(conn)
+        yield ls
+    finally:
+        await conn.close()
+
+
 def main() -> None:
-    worker = hatchet.worker(name="incident-bot", workflows=[simple])
+    worker = hatchet.worker(name="incident-bot", workflows=[simple], lifespan=lifespan)
     worker.start()
 
 
